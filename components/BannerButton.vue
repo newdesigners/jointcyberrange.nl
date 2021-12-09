0cba84ab4c77fbbed08c975902750b2bb95eb15a
Steven Wu <master_msp1@hotmail.com>
2021-12-09 16:31:33 +0100

    add padding for titles and text

--- components/BannerButton.vue
+++ components/BannerButton.vue
@@ -5,10 +5,10 @@
     <div class="container">
       <article class="py-10 lg:py-20 sm:w-3/4 mx-auto flex flex-col items-center">
         <h2 class="pb-5">{{ blok.title }}</h2>
-        <div v-if="blok.description">
+        <div class="mb-5" v-if="blok.description">
           <rich-text-renderer :document="blok.description" />
         </div>
-        <Button class="mt-5 lg:mt-10" :label="blok.button_label" :button="blok.button_url" />
+        <Button :label="blok.button_label" :button="blok.button_url" />
       </article>
     </div>
   </div>
