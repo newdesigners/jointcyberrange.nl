475c13662c44051fa15dc360f3a234ec901a0728
Tanveer-Khawaja <6014321@mborijnland.nl>
2021-12-13 10:31:18 +0100

    text-image component

--- components/TextImage.vue
+++ components/TextImage.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <article>
-      <div>
-        <h2>{{ blok.title }}</h2>
-        <rich-text-renderer :document="blok.description" />
+    <article class="container xl:flex l:flex">
+      <div class="w-9/12" >
+        <h2 class="w-full pb-5">{{blok.title}}</h2>
+        <div class="rich-text-renderer">
+        <rich-text-renderer class="mt-5 lg:w-1/2" :document="blok.description" />
+        </div>
       </div>
-      <figure v-if="blok.image.filename">
+      <figure class="xl:w-1/2 ml-10"  v-if="blok.image.filename"> 
         <img :src="blok.image.filename" :alt="blok.image.alt">
       </figure>
     </article>
