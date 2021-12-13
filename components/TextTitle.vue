6a04bed4c6c5c0af8164fc7c33bed426dff1ea2f
Agency <agency@hhsdtp5134simac.wireless.hhs.nl>
2021-12-13 17:31:15 +0100

    text title done

---
+++ components/TextTitle.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-editable="blok">
+    <article class="mx-auto container py-10 text-center lg:w-3/4">
+      <h2 class="text-24 lg:text-36 pb-5 lg:pb-9">{{ blok.title }}</h2>
+      <p class="text-left">{{ blok.description }}</p>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    blok: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
