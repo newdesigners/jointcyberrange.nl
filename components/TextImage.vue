e970e73f37fde931f219dcc56d60f503cd28193d
Tanveer-Khawaja <6014321@mborijnland.nl>
2021-12-14 12:23:03 +0100

    responsive text-image

--- components/TextImage.vue
+++ components/TextImage.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <article class="container xl:flex l:flex">
+    <article class="container xl:flex lg:flex">
       <div class="w-9/12" >
         <h2 class="w-full pb-5">{{blok.title}}</h2>
         <div class="rich-text-renderer">
-        <rich-text-renderer class="mt-5 lg:w-1/2" :document="blok.description" />
+        <rich-text-renderer class="mt-5 lg:w-1/2 sm:w-full" :document="blok.description" />
         </div>
       </div>
-      <figure class="xl:w-1/2 ml-10"  v-if="blok.image.filename"> 
+      <figure class="xl:w-1/2 lg:ml-10"  v-if="blok.image.filename"> 
         <img :src="blok.image.filename" :alt="blok.image.alt">
       </figure>
     </article>
