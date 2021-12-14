b34abaff6d70606eaf83a21f0d8e8c36822d664e
Agency <agency@hhsdtp5132simac.wireless.hhs.nl>
2021-12-14 14:00:36 +0100

    fixed merge conflicts

--- components/TextImage.vue
+++ components/TextImage.vue
@@ -1,25 +1,13 @@
 <template>
-<<<<<<< HEAD
-  <div>
-    <article class="container xl:flex lg:flex">
-=======
   <div v-editable="blok"> 
-    <article class="container xl:flex l:flex">
->>>>>>> 2c7cfadd57deec73aebb8dab67a081e4e737b8dc
+    <article class="container lg:flex">
       <div class="w-9/12" >
         <h2 class="w-full pb-5">{{ blok.title }}</h2>
         <div class="rich-text-renderer">
-<<<<<<< HEAD
-        <rich-text-renderer class="mt-5 lg:w-1/2 sm:w-full" :document="blok.description" />
-        </div>
-      </div>
-      <figure class="xl:w-1/2 lg:ml-10"  v-if="blok.image.filename"> 
-=======
           <rich-text-renderer class="mt-5 lg:w-1/2" :document="blok.description" />
         </div>
       </div>
       <figure class="xl:w-1/2 ml-10" v-if="blok.image.filename"> 
->>>>>>> 2c7cfadd57deec73aebb8dab67a081e4e737b8dc
         <img :src="blok.image.filename" :alt="blok.image.alt">
       </figure>
     </article>
