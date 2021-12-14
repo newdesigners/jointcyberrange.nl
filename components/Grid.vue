166af75742c4e6dcb6637cd891b19e5b465b2ed1
Agency <agency@hhsdtp5134simac.wireless.hhs.nl>
2021-12-14 10:11:38 +0100

    grid icon text is fixed

--- components/Grid.vue
+++ components/Grid.vue
@@ -5,9 +5,9 @@
     <div class="container">
       <article class="py-10 lg:py-28 sm:w-3/4 mx-auto text-center">
         <h2 class="pb-10 lg:pb-14" v-if="blok.title !== ''">{{ blok.title }}</h2>
-        <ul class="flex flex-wrap gap-8 justify-around sm:justify-between">
-          <li class="max-w-grid-icon-text w-2/5 sm:w-1/4 flex items-center justify-center" :class="{ 'pb-2.5 lg:pb-5' : item.component === 'logo' }" v-for="item in blok.items" :key="item._uid">
-            <div v-if="item.component === 'icon_text'">
+        <ul class="flex flex-wrap items-start justify-between">
+          <li class="max-w-grid-icon-text w-32% sm:w-1/4 flex items-center justify-center" :class="{ 'pb-2.5 lg:pb-5' : item.component === 'logo' }" v-for="item in blok.items" :key="item._uid">
+            <div class="w-full" v-if="item.component === 'icon_text'">
               <Resources class="w-full pb-2.5 lg:pb-10 max-w-grid-icon-text-icon lg:max-w-grid-icon-text-icon-lg mx-auto" :type="item.icon" />
               <p class="text-white text-center break-words xl:text-36">{{ item.text }}</p>
             </div>
