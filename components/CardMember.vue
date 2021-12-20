24d5cdcbf84ae8cf5376e7d10e489a15ee8941a2
Steven Wu <master_msp1@hotmail.com>
2021-12-20 16:48:22 +0100

    team component done

--- components/CardMember.vue
+++ components/CardMember.vue
@@ -1,25 +1,27 @@
 <template>
   <div
-    class="lg:flex lg:justify-end"
+    class="lg:flex lg:justify-center card"
     v-editable="blok"
   >
     <div class="bg-background relative shadow-card-member lg:w-11/12">
-      <div class="w-full relative px-2.5 flex mt-10 min-h-36 card__header">
-        <figure class="absolute left-0 -top-10 w-36 h-40 lg:w-80 lg:h-96 lg:-left-20 lg:-top-32" v-if="blok.image.filename">
+      <div class="w-full relative px-2.5 lg:px-10 pl-40 lg:pl-80 flex mt-10 min-h-36 card__header">
+        <figure class="absolute left-0 -top-10 w-36 h-40 lg:w-[312px] lg:h-[352px] lg:-left-10 lg:-top-32" v-if="blok.image.filename">
           <img
             class="w-full h-full object-cover"
             :src="blok.image.filename"
             :alt="blok.image.alt" 
           />
         </figure>
-        <div class="w-full flex-1 pt-3 pl-[9.25rem] card__body">
+        <div class="w-full flex-1 pt-3 card__body">
           <h2 class="text-18 lg:text-32 text-green pb-2.5">{{ blok.name }}</h2>
-          <h3 class="break-words text-14 lg:text-20 sm:max-w-[22rem]">{{ blok.title }}</h3>
+          <h3 class="break-words text-14 lg:text-20 sm:max-w-[22rem] lg:max-w-none">{{ blok.title }}</h3>
         </div>
       </div>
-      <aside class="px-4 pt-4">
-        <p class="pb-8">{{ blok.description }}</p>
-        <q class="inline-block pb-8 text-16 lg:text-20 italic">{{ blok.quote }}</q>
+      <aside class="px-4 pt-8 lg:px-10 xl:pl-80 sm:pt-12 lg:pt-40 xl:pt-12">
+        <article class="">
+          <p class="pb-8">{{ blok.description }}</p>
+          <q class="inline-block pb-8 text-16 lg:pb-10 lg:text-20 italic">{{ blok.quote }}</q>
+        </article>
       </aside>
     </div>
   </div>
