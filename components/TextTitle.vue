6c2f2e04a9ede9474fa22cb06f7cdf2e37a90300
Steven Wu <master_msp1@hotmail.com>
2021-12-27 20:25:57 +0100

    text-image-overlap done

--- components/TextTitle.vue
+++ components/TextTitle.vue
@@ -3,7 +3,7 @@
     class="relative background__highlight"
     v-editable="blok"
   >
-    <article class="mx-auto container py-10 text-center sm:w-4/5">
+    <article class="mx-auto container py-10 text-left lg:text-center sm:w-4/5">
       <h2 class="text-24 lg:text-36 pb-5 lg:pb-9">{{ blok.title }}</h2>
       <p class="text-left">{{ blok.description }}</p>
     </article>
