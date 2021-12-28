20dbcc32bfac831d26ef5240cd9cedbadbc1f8ac
Steven Wu <master_msp1@hotmail.com>
2021-12-28 19:19:55 +0100

    edit for what is cyberrange page

--- components/TextImage.vue
+++ components/TextImage.vue
@@ -1,9 +1,13 @@
 <template>
   <div
     v-editable="blok"
-    class="pb-5 lg:pb-10 xl:pb-32"
+    class="pb-5 lg:pb-10 xl:pb-32 relative"
+    :class="{ 'background__highlight background__highlight--top' : blok.background_highlight_circle, 'background__highlight--left background__highlight--top' : blok.reverse }"
   > 
-    <article class="container lg:flex gap-14 justify-between">
+    <article 
+      class="container lg:flex gap-14 justify-between"
+      :class="{ 'lg:flex-row-reverse' : blok.reverse }"
+    >
       <div class="w-full lg:w-1/2 lg:flex-1">
         <h2 class="w-full pb-5">{{ blok.title }}</h2>
         <div class="rich-text-renderer">
@@ -11,11 +15,11 @@
         </div>
       </div>
       <figure
-        class="lg:w-1/2 xl:w-[565px]"
+        class="lg:w-1/2 xl:w-[565px] xl:min-h-[450px]"
         v-if="blok.image.filename"
       > 
         <img
-          class="w-full"
+          class="w-full h-full object-fit"
           :src="blok.image.filename"
           :alt="blok.image.alt"
         />
--- components/TextTitle.vue
+++ components/TextTitle.vue
@@ -4,9 +4,16 @@
     :class="{ 'background__highlight' : blok.background_highlight_circle }"
     v-editable="blok"
   >
-    <article class="mx-auto container py-10 text-left lg:text-center sm:w-4/5">
-      <h2 class="text-24 lg:text-36 pb-5 lg:pb-9">{{ blok.title }}</h2>
-      <p class="text-left">{{ blok.description }}</p>
+    <article
+      class="mx-auto container pb-10 pt-0"
+      :class="{ 'pb-16 lg:pb-36' : blok.title === ''}"
+    >
+      <div class="sm:w-4/5 mx-auto max-w-[900px]">
+        <h2 
+          v-if="blok.title !== ''"
+          class="text-24 lg:text-36 pb-5 lg:pb-9">{{ blok.title }}</h2>
+        <p class="text-left">{{ blok.description }}</p>
+      </div>
     </article>
   </div>
 </template>
