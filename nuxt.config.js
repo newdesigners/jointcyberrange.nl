95055f62f8b9a67468e4b71d6df9191c5c5dd5c2
Steven Wu <master_msp1@hotmail.com>
2022-01-10 23:25:52 +0100

    add favico

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,9 +25,16 @@ export default {
         },
       }),
       { name: 'format-detection', content: 'telephone=no' },
+      { name: 'theme-color', content: '#01102c' },
+      { name: 'msapplication-TileColor', content: '#01102c' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type:'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type:'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#01102c' },
     ]
   },
 
