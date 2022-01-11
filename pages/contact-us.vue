c633568a0a8976600ce1ab2eac8c55c315b62655
Tanveer-Khawaja <6014321@mborijnland.nl>
2022-01-11 16:47:38 +0100

    contact page begin aan de formulier

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -1,7 +1,19 @@
 <template>
   <section>
     <div class="container">
-      <h1>Contact us page</h1>
+      <h2 class="text-center pb-10">Contact</h2>
+      <p class="text-center w-full">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+   
+    <div class="contact-box">
+			<div class="left"></div>
+			<div class="right">
+				<input class="" type="text" placeholder="First name">
+				<input class="" placeholder="Second name">
+				<input class="flex" placeholder="E-mail">
+				<textarea placeholder="Message" class="field"></textarea>
+				<button class="btn">Submit</button>
+			</div>
+		</div>
     </div>
   </section>
 </template>
