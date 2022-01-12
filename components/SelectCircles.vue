4b246fef28cb67c4472645fe660e69f4e289b5da
Steven Wu <master_msp1@hotmail.com>
2022-01-12 18:13:33 +0100

    add href functionality to button

--- components/SelectCircles.vue
+++ components/SelectCircles.vue
@@ -11,9 +11,26 @@
             v-for="selector in blok.selectors"
             :key="selector._uid"
           >
-            <div class="mx-auto w-36 h-36 md:w-52 md:h-52 lg:w-72 lg:h-72 bg-blue-background rounded-full flex items-center shadow-card-member justify-center">
-              <h2 class="text-18 md:text-24 lg:text-36 text-center">{{ selector.title }}</h2>
-            </div>
+            <a
+              class="block" 
+              v-if="selector.link_url.linktype === 'url'"
+              :href="selector.link_url.url"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div class="mx-auto w-36 h-36 md:w-52 md:h-52 lg:w-72 lg:h-72 bg-blue-background rounded-full flex items-center shadow-card-member justify-center">
+                <h2 class="text-18 md:text-24 lg:text-36 text-center">{{ selector.title }}</h2>
+              </div>
+            </a>
+            <NuxtLink
+              class="block"
+              v-if="selector.link_url.linktype === 'story'"
+              :to="selector.link_url.cached_url"
+            >
+              <div class="mx-auto w-36 h-36 md:w-52 md:h-52 lg:w-72 lg:h-72 bg-blue-background rounded-full flex items-center shadow-card-member justify-center">
+                <h2 class="text-18 md:text-24 lg:text-36 text-center">{{ selector.title }}</h2>
+              </div>
+            </NuxtLink>
           </li>
         </ul>
       </div>
