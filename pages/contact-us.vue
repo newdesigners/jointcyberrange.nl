8a111d1af4778fbe328cf1e2d7272654b7e7e191
Tanveer-Khawaja <6014321@mborijnland.nl>
2022-01-12 16:40:37 +0100

    contact page almost done, styling left

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -2,18 +2,19 @@
   <section>
     <div class="container">
       <h2 class="text-center pb-10">Contact</h2>
-      <p class="text-center w-full">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+      <p class="text-center w-full pb-10">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
    
-    <div class="contact-box">
-			<div class="left"></div>
+    <div class="style.scss">
+		<div class="contact-box">
 			<div class="right">
-				<input class="" type="text" placeholder="First name">
-				<input class="" placeholder="Second name">
-				<input class="flex" placeholder="E-mail">
+				<input class="field" placeholder="First Name">
+				<input class="field" placeholder="Last Name">
+				<input class="field" placeholder="E-mail">
 				<textarea placeholder="Message" class="field"></textarea>
-				<button class="btn">Submit</button>
 			</div>
+			<button class="btn">Submit</button>
 		</div>
+	</div>
     </div>
   </section>
 </template>
