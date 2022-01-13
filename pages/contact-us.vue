e1519f5c261798d32b5b6328caf43eb1cf327816
Tanveer-Khawaja <6014321@mborijnland.nl>
2022-01-13 16:28:53 +0100

    fixed styling, responsive left

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -2,17 +2,23 @@
   <section>
     <div class="container">
       <h2 class="text-center pb-10">Contact</h2>
-      <p class="text-center w-full pb-10">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+      <p class="text-center w-full pb-20">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
    
+	<div class="flex">
     <div class="style.scss">
-		<div class="contact-box">
 			<div class="right">
-				<input class="field" placeholder="First Name">
-				<input class="field" placeholder="Last Name">
-				<input class="field" placeholder="E-mail">
-				<textarea placeholder="Message" class="field"></textarea>
+				<input class="field-first-name" placeholder="First Name">
+				<input class="field-last-name" placeholder="Last Name">
 			</div>
+			<div>
+				<input class="field-e-mail" placeholder="E-mail">
+			<div>
+				<textarea placeholder="Message" class="field-message"></textarea>
+			</div>
+			</div>
+			<div>
 			<button class="btn">Submit</button>
+			</div>
 		</div>
 	</div>
     </div>
